66caa1115df335c335a8efbf5d20a698f42feded
ketanprakash <ketanprakash001@gmail.com>
2021-08-14 21:58:15 +0530

    ok

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const githubImage = document.querySelector('.github-image');
 const linkedInImage = document.querySelector('.linked-in-image');
 const aboutLink = document.querySelector('.about-link');
 const projectLink = document.querySelector('.project-link');
+const skillsLink = document.querySelector('.skills-link');
 const nav = document.querySelector('nav');
 const aboutSection = document.querySelector('.about');
 const projectsSection = document.querySelector('.projects');
+const skillsSection = document.querySelector('.skills');
 const navSmall = document.querySelector('.nav-small');
 
 const main = document.querySelector('main');
@@ -19,28 +21,44 @@ linkedInImage.addEventListener('click', () => {
 
 aboutLink.addEventListener('click', () => {
   projectLink.classList.remove('nav-link-active');
+  skillsLink.classList.remove('nav-link-active');
   aboutLink.classList.add('nav-link-active');
+  skillsSection.classList.add('does-not-exist');
   projectsSection.classList.add('does-not-exist');
   aboutSection.classList.remove('does-not-exist');
 })
 
 projectLink.addEventListener('click', () => {
   aboutLink.classList.remove('nav-link-active');
+  skillsLink.classList.remove('nav-link-active');
   projectLink.classList.add('nav-link-active');
+  skillsSection.classList.add('does-not-exist');
   projectsSection.classList.remove('does-not-exist');
   aboutSection.classList.add('does-not-exist');
+});
+
+skillsLink.addEventListener('click', () => {
+  aboutLink.classList.remove('nav-link-active');
+  projectLink.classList.remove('nav-link-active');
+  skillsLink.classList.add('nav-link-active');
+  aboutSection.classList.add('does-not-exist');
+  projectsSection.classList.add('does-not-exist');
+  skillsSection.classList.remove('does-not-exist');
 });
 
 let x = window.matchMedia("(max-width: 768px)");
 
 if (x.matches){
-  nav.classList.add('does-not-exist');
+  // nav.classList.add('does-not-exist');
+  nav.classList.add('invisible');
   main.addEventListener('click', (e)  => {
-    nav.classList.add('does-not-exist');
+    // nav.classList.add('does-not-exist');
+    nav.classList.add('invisible');
   })  
 }
 
 navSmall.addEventListener('click', () => {
-  nav.classList.toggle('does-not-exist');
+  // nav.classList.toggle('does-not-exist');
+  nav.classList.toggle('invisible');
   // console.log(nav);
 })
